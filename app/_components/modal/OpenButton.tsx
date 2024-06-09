742ff38a3f7c34ea6b45223ea54c9c5497b241ad
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-09 16:45:53 +0800

    cleanup modal

--- app/_components/modal/OpenButton.tsx
+++ app/_components/modal/OpenButton.tsx
@@ -1,29 +1,20 @@
 import useModal from '@/app/_hooks/useModal'
 import type { TOpenButtonProps } from '@/app/_types/modal.types'
-import SpinnerMini from '../SpinnerMini'
 
 const OpenButton = ({
   className = '',
   windowNameToOpen,
   children
 }: TOpenButtonProps) => {
-  const { openWindowWithName, isPending } = useModal()
+  const { openWindowWithName } = useModal()
 
   return (
-    <>
-      {isPending ? (
-        <span className={`inline-flex ${className}`}>
-          <SpinnerMini />
-        </span>
-      ) : (
-        <button
-          className={`inline-flex ${className}`}
-          onClick={() => openWindowWithName(windowNameToOpen)}
-        >
-          {children}
-        </button>
-      )}
-    </>
+    <button
+      className={`inline-flex ${className}`}
+      onClick={() => openWindowWithName(windowNameToOpen)}
+    >
+      {children}
+    </button>
   )
 }
 
