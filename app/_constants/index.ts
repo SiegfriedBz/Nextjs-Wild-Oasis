7569a732efbf9880ca08e34d0c3574e9573e28e4
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-09 13:17:49 +0800

    add constants

---
+++ app/_constants/index.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+  { href: '/cabins', label: 'Cabins' },
+  { href: '/about', label: 'About us' },
+  { href: '/account', label: 'Guest area' }
+]
+
+export const MOBILE_NAV_LINKS = [{ href: '/', label: 'Home' }, ...NAV_LINKS]
